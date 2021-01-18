59d54f8a7c1f659bbf351236ff45cc21ec6fff5c
MikeYef96 <mike.yef@gmail.com>
2021-01-18 15:36:58 +0300

    modal.js updetes

--- js/modal.js
+++ js/modal.js
@@ -1,17 +1,15 @@
 (() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+  const openModalBtn = document.querySelector("[data-modal-open]");
+  const closeModalBtn = document.querySelector("[data-modal-close]");
+  const modal = document.querySelector("[data-modal]");
+
+  const toggleModal = () => {
+    document.body.classList.toggle("modal-open");
+    modal.classList.toggle("is-hidden");
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    document.body.classList.toggle('modal-open')
-    refs.modal.classList.toggle('is-hidden');
-  }
+  openModalBtn.addEventListener("click", toggleModal);
+  closeModalBtn.addEventListener("click", toggleModal);
 })();
 
 // const { height: pageHeaderHeight } = document
